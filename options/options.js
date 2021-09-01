b51af989abbb7af885611d21317db3749124c7f6
Crowdbotics <team@crowdbotics.com>
2021-09-01 07:58:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "odd_grass_30242Identifier",
+  url: "https://odd_grass_30242Identifier.botics.co",
+  api: "https://odd_grass_30242Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
